ec1ba7a698e7bd4036e5b777b62181d2a4a8baf9
Vinaal R <vinaalm898@gmail.com>
2026-08-11 10:18:17 +0530

    Create AppComponent for Angular application

---
+++ app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  template: `
+    <main>
+      <h1>Hello World!</h1>
+      <p>Welcome to your first Angular page.</p>
+    </main>
+  `
+})
+export class AppComponent {}
